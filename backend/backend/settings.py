24d39b043ce2e09a500ee9236eabe90cf3dbf79c
Alexander Mikhailov <extroper@yandex.ru>
2024-09-15 14:22:58 +0300

    Up to Sprint 17; Theme 02; Lesson 10

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,12 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '89.169.161.209',
+    '127.0.0.1',
+    'localhost',
+    'avtomatik.sytes.net',
+]
 
 
 # Application definition
@@ -109,6 +114,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'collected_static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
